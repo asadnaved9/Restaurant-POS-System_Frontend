017d997bfe5a4da089eace2c8400dd1e8f5404bd
Asad Naved <asadnaved4@gmail.com>
2026-04-02 23:35:31 +0530

    First commit(Home page UI done)

---
+++ src/components/home/OrderList.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { FaCheckDouble, FaLongArrowAltRight } from "react-icons/fa";
+import { FaCircle } from "react-icons/fa";
+// import { getAvatarName } from "../../utils/index";
+
+const OrderList = () => {
+  return (
+    <div className="flex items-center gap-5 mb-3">
+      <button className="bg-[#f6b100] p-3 text-xl font-bold rounded-lg">
+        AN
+      </button>
+      <div className="flex items-center justify-between w-[100%]">
+        <div className="flex flex-col items-start gap-1">
+          <h1 className="text-[#f5f5f5] text-lg font-semibold tracking-wide">
+            Asad Naved
+          </h1>
+          <p className="text-[#ababab] text-sm">8 Items</p>
+        </div>
+
+        <h1 className="text-[#f6b100] font-semibold border border-[#f6b100] rounded-lg p-1">
+          Table No:3<FaLongArrowAltRight className="text-[#ababab] ml-2 inline" />
+        </h1>
+
+        <div className="flex flex-col items-end gap-2">
+          
+        
+              <p className="text-green-600 bg-[#2e4a40] px-2 py-1 rounded-lg">
+                <FaCheckDouble className="inline mr-2" /> Ready
+              </p>
+            
+          
+            
+              <p className="text-yellow-600 bg-[#4a452e] px-2 py-1 rounded-lg">
+                <FaCircle className="inline mr-2" /> Ready to Serve
+              </p>
+            
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OrderList;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import BottonNav from '../components/shared/BottomNav'
+import Greetings from '../components/home/Greetings'
+import MiniCard from '../components/home/MiniCard'
+import RecentOrders from '../components/home/RecentOrders'
+import PopularDishes from '../components/home/PopularDishes'
+import { BsCashCoin } from "react-icons/bs";
+import { GrInProgress } from "react-icons/gr";
+
+
+const Home = () => {
+  return (
+     <section className="bg-[#1f1f1f]  h-[calc(100vh-5rem)] overflow-hidden flex gap-3">
+      {/* Left Div */}
+      <div className="flex-[3]"><Greetings />
+        <div className="flex items-center w-full gap-3 px-8 mt-8">
+          <MiniCard title="Total Earnings" icon={<BsCashCoin />} number={512} footerNum={1.6} />
+          <MiniCard title="In Progress" icon={<GrInProgress />} number={16} footerNum={3.6} />
+        </div>
+        <RecentOrders/>
+      </div>
+       
+      {/* Right Div */}
+      <div className="flex-[1]">
+        <PopularDishes/>
+      </div>
+
+      <BottonNav/> 
+     </section>
+  )
+}
+
+export default Home
