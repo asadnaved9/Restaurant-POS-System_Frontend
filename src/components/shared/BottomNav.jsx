8e3603fc3918722ab8db49ae32efcbbc478f0516
Asad Naved <asadnaved4@gmail.com>
2026-04-03 03:33:00 +0530

    Order page UI done

--- src/components/shared/BottomNav.jsx
+++ src/components/shared/BottomNav.jsx
@@ -4,13 +4,17 @@ import { MdOutlineReorder } from "react-icons/md";
 import { MdTableBar } from "react-icons/md";
 import { CiCircleMore } from "react-icons/ci";
 import { BiSolidDish } from "react-icons/bi";
+import { useNavigate } from "react-router-dom";
 
 const BottonNav = () => {
+
+    const navigate = useNavigate();
+
   return (
     <div className="fixed bottom-0 left-0 right-0 bg-[#262626] p-2 h-16 flex justify-around">
-        <button className='flex items-center justify-center text-[#f5f5f5] bg-[#343434] w-[200px] rounded-[20px]'><FaHome className="inline mr-2" size={20} /> <p>Home</p></button>
-        <button className='flex items-center justify-center text-[#ababab] w-[200px]'><MdOutlineReorder className="inline mr-2" size={20} /> <p>Orders</p></button>
-        <button className='flex items-center justify-center text-[#ababab] w-[200px]'><MdTableBar className="inline mr-2" size={20} /> <p>Table</p></button>
+        <button onClick={() => navigate("/")} className='flex items-center justify-center text-[#f5f5f5] bg-[#343434] w-[200px] rounded-[20px]'><FaHome className="inline mr-2" size={20} /> <p>Home</p></button>
+        <button onClick={() => navigate("/orders")} className='flex items-center justify-center text-[#ababab] w-[200px]'><MdOutlineReorder className="inline mr-2" size={20} /> <p>Orders</p></button>
+        <button onClick={() => navigate("/table")} className='flex items-center justify-center text-[#ababab] w-[200px]'><MdTableBar className="inline mr-2" size={20} /> <p>Table</p></button>
         <button className='flex items-center justify-center text-[#ababab] w-[200px]'><CiCircleMore className="inline mr-2" size={20} /> <p>More</p></button>
         <button className="absolute bottom-6 bg-[#F6B100] text-[#f5f5f5] rounded-full p-4 items-center"><BiSolidDish size={30} /> </button>
     </div>
